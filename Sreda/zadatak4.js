87f58b81157c6a55bd7ca3cfdfb1f953b8250cea
MajaBozic <47162007+MajaBozic@users.noreply.github.com>
2020-07-25 00:31:39 +0200

    izmene4

--- Sreda/zadatak4.js
+++ Sreda/zadatak4.js
@@ -1,8 +1,10 @@
 // Ispisati prvih 1000 brojeva deljivih sa 5
 
 let x
-for (x = 1; x <= 5000; x++) {
+
+for (x = 1, i = 0; i < 1000; x++) {
     if (x % 5 === 0) {
-    console.log(x);
+    console.log(x)
+    i++
     }
 }
